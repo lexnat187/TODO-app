ca6d135ab7b695a0baf17c40ff6b26577ee5b84f
Alexander Walker <alexwalker1@live.co.uk>
2018-01-29 16:09:01 +0400

    framework scaffolding

---
+++ src/main/java/com/awalker/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package com.awalker.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+	    SpringApplication.run(TodoApplication.class, args);
+	}
+}
